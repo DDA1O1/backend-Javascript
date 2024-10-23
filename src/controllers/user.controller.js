4f9816d35cf7b21ed353d78d73815310fcb681ce
debashish <debashishdash03412@gmail.com>
2024-10-23 19:08:53 +0530

    commented the whole code in controllere.js and added a simple respone

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,57 +1,68 @@
 import {asyncHandler} from "../utils/asyncHandler.js";
 
-const registerUser = asyncHandler(async (req, res) => {
-    const { username, fullname, email, password } = req.body;
-    if (!username || !fullname || !email || !password) {
-        res.status(400);
-        throw new Error("Please add all fields");
-    }
-    const userExists = await User.findOne({ email });
-    if (userExists) {
-        res.status(400);
-        throw new Error("User already exists");
-    }
-    const user = await User.create({
-        username,
-        fullname,
-        email,
-        password,
-    });
-    if (user) {
-        res.status(201).json({
-            _id: user.id,
-            username: user.username,
-            fullname: user.fullname,
-            email: user.email,
-        });
-    } else {
-        res.status(400);
-        throw new Error("Invalid user data");
-    }
-});
 
-const loginUser = asyncHandler(async (req, res) => {
-    const { email, password } = req.body;
-    if (!email || !password) {
-        res.status(400);
-        throw new Error("Please add email and password");
-    }
-    const user = await User.findOne({ email });
-    if (user && (await user.isPasswordMatched(password))) {
-        res.json({
-            _id: user.id,
-            username: user.username,
-            fullname: user.fullname,
-            email: user.email,
-            accessToken: user.generateAccessToken(),
+const registerUser = asyncHandler(async (req, res) => {
+    res.status(200).json(
+        {
+            message: "User registered successfully"
         });
-    } else {
-        res.status(401);
-        throw new Error("Invalid email or password");
-    }
-});
+})
 
 export {
-    registerUser,
-    loginUser,
-};
+    registerUser
+}
+// const registerUser = asyncHandler(async (req, res) => {
+//     const { username, fullname, email, password } = req.body;
+//     if (!username || !fullname || !email || !password) {
+//         res.status(400);
+//         throw new Error("Please add all fields");
+//     }
+//     const userExists = await User.findOne({ email });
+//     if (userExists) {
+//         res.status(400);
+//         throw new Error("User already exists");
+//     }
+//     const user = await User.create({
+//         username,
+//         fullname,
+//         email,
+//         password,
+//     });
+//     if (user) {
+//         res.status(201).json({
+//             _id: user.id,
+//             username: user.username,
+//             fullname: user.fullname,
+//             email: user.email,
+//         });
+//     } else {
+//         res.status(400);
+//         throw new Error("Invalid user data");
+//     }
+// });
+
+// const loginUser = asyncHandler(async (req, res) => {
+//     const { email, password } = req.body;
+//     if (!email || !password) {
+//         res.status(400);
+//         throw new Error("Please add email and password");
+//     }
+//     const user = await User.findOne({ email });
+//     if (user && (await user.isPasswordMatched(password))) {
+//         res.json({
+//             _id: user.id,
+//             username: user.username,
+//             fullname: user.fullname,
+//             email: user.email,
+//             accessToken: user.generateAccessToken(),
+//         });
+//     } else {
+//         res.status(401);
+//         throw new Error("Invalid email or password");
+//     }
+// });
+
+// export {
+//     registerUser,
+//     loginUser,
+// };
