94e0ccc7a932220e5122a3ea809d532e07d848f6
debashish <debashishdash03412@gmail.com>
2024-10-23 18:26:30 +0530

    added cloudinary and multer

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export const uploadOnCloudinaryImage = async (localFilePath) => {
+    try {
+        if (!localFilePath) {
+            return new Error("localFilePath not found");
+        }
+        // upload image to cloudinary
+        const uploadResult = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        });
+        // file has been uploaded to cloudinary
+        console.log("file has been uploaded to cloudinary", uploadResult.url);
+
+        return uploadResult.url;
+        
+    } catch (error) {
+        fs.unlinkSync(localFilePath); // remove the locally uploaded file as the upload to cloudinary failed
+        return null;
+    }
+};
+      
